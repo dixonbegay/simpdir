a65a16727d21ca89fdea58e702410ea5780a3643
Dixon Begay <dixon@dixon-labs.net>
2026-05-11 21:01:56 -0700

    Removed old commented code.

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -20,8 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("dashboard.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("simpdir_phonedir.urls")),
-    # path('', include('phonedir.urls'))
 ]
