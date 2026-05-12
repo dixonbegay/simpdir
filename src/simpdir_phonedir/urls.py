eda432399bbe33d9c09cdccca06a7d9f554162b6
Dixon Begay <dixon@dixon-labs.net>
2026-05-11 21:07:51 -0700

    Relying on built-in templates and URL paths provided by django-phonedir.

--- src/simpdir_phonedir/urls.py
+++ src/simpdir_phonedir/urls.py
@@ -1,19 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
-from .views import (  # Assuming views.py is in the same app folder
-    DepartmentDetailView,
-    # DepartmentListView,
-    SearchResultsView,
-    home_view,
-)
+from .views import home_view
 
 urlpatterns = [
     path("", home_view, name="home"),
-    # path("departments", DepartmentListView.as_view(), name="department_list"),
-    path(
-        "department/<slug:short_name>/",
-        DepartmentDetailView.as_view(),
-        name="department_detail",
-    ),
-    path("search/", SearchResultsView.as_view(), name="search_results"),
+    path("", include("django_phonedir.urls")),
 ]
