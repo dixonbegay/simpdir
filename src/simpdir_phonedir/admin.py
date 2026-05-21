ab40bc3ef6178326293322835183c5557ba65611
Dixon Begay <dixon@dixon-labs.net>
2026-05-20 19:42:48 -0700

    Changed what Admin portal header, title, and index_title states.

---
+++ src/simpdir_phonedir/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Simple and Important Managed Phone Directory"
+admin.site.site_title = "SimpDir"
+admin.site.index_title = "Admin Portal"
